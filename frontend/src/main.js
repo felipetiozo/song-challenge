800d9f728db3d1ade56ae46f8c49df4e1631726e
FeoSilva <felipeo.silva@hotmail.com>
2020-11-07 20:26:18 -0300

    Add Vant UI and Vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,16 @@
 import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import App from "@/App.vue";
+import router from "@/router";
+import vant from "@/plugins/vant";
+import vuetify from "@/plugins/vuetify";
+import store from "@/store";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vant,
+  vuetify,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
