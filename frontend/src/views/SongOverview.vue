ae6167b908066db071d52f5e6961de0becc3faa6
FeoSilva <felipeo.silva@hotmail.com>
2020-11-07 20:49:29 -0300

    Starts song overview page

---
+++ frontend/src/views/SongOverview.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container class="song-overview-page">
+    <v-row>
+      <v-btn icon>
+        <v-icon color="white">mdi-close</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "SongOverview",
+};
+</script>
+<style scoped>
+.song-overview-page {
+  background: #424b54;
+  min-height: 100vh;
+}
+</style>
